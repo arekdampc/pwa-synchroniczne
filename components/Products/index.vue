dfafa5e483c4bc969b5756eddfb5299572520b22
AMilewczyk <32812906+AMilewczyk@users.noreply.github.com>
2024-04-13 17:08:52 +0200

    dodanie klikania

--- components/Products/index.vue
+++ components/Products/index.vue
@@ -20,25 +20,31 @@
     <div class="product-row">
 
       <div class="product">
+        <a href="https://www.x-kom.pl/p/1222216-smartfon-telefon-xiaomi-14-ultra-16-512gb-black.html">
         <img 
         src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/2/pr_2024_2_9_14_15_27_92_05.jpg"
          loading="lazy"
           alt="Smartfon / Telefon Xiaomi 14 Ultra 16/512GB Black" 
-          class="img">
+          class="img"
+          >
           <p>Xiaomi 14 Ultra 16/512GB Black</p>
           <p>6 799,00 zł</p>
+        </a>
       </div>
 
       <div class="product">
+      <a href="https://www.x-kom.pl/p/1220984-smartfon-telefon-nothing-phone-2a-5g-12-256gb-black-120hz.html">
         <img
         src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/3/pr_2024_3_1_11_29_50_969_04.jpg"
         loading="lazy" alt="Smartfon / Telefon Xiaomi 14 Ultra 16/512GB Black"
         class="img">
           <p>Nothing Phone (2a) 5G 12/256GB Black</p>
           <p>1 679,00 zł</p>
+        </a>
       </div>
 
       <div class="product">
+        <a href="https://www.x-kom.pl/p/1227961-notebook-laptop-136-apple-macbook-air-m3-8gb-256-mac-os-polnoc-8r-gpu.html">
         <img 
           src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/3/pr_2024_3_5_8_41_3_896_00.jpg" 
           loading="lazy" 
@@ -46,9 +52,11 @@
           class="img">
           <p>Apple MacBook Air M3/8GB/256/Mac</p>
           <p>5 999,00 zł</p>
+        </a>
       </div>
 
       <div class="product">
+        <a href="https://www.x-kom.pl/p/1198619-konsola-playstation-sony-playstation-5-digital-d-chassis.html">
         <img 
           src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2023/11/pr_2023_11_23_11_12_58_599_00.jpg" 
           loading="lazy" 
@@ -56,9 +64,11 @@
           class="img">
           <p>Sony PlayStation 5 D Chassis</p>
           <p>2 699,00 zł</p>
+        </a>
       </div>
 
       <div class="product">
+        <a href="https://www.x-kom.pl/p/1162620-telewizor-55-59-philips-55pml9008-55-miniled-4k-120hz-ambilight-3-dolby-atmos.html">
         <img 
           src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2023/9/pr_2023_9_27_11_57_25_663_00.jpg" 
           loading="lazy" 
@@ -66,9 +76,11 @@
           class="img">
           <p>Philips 55PML9008 55" MINILED</p>
           <p>3 999,00 zł</p>
+        </a>
       </div>
 
       <div class="product">
+        <a href="https://www.x-kom.pl/p/1072100-pamiec-ram-ddr5-kingston-fury-32gb-2x16gb-5600mhz-cl36-beast-rgb-expo-amd.html">
         <img 
           src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2024/3/pr_2024_3_4_8_47_10_729_00.jpg" 
           loading="lazy" 
@@ -76,9 +88,11 @@
           class="img">
           <p>Kingston FURY 32GB</p>
           <p>889,00 zł</p>
+        </a>
       </div>
 
       <div class="product">
+        <a href="https://www.x-kom.pl/p/1184562-obudowa-do-komputera-phanteks-nv-serie-nv5-white.html">
         <img 
           src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2023/10/pr_2023_10_5_12_59_33_19_00.jpg"
           loading="lazy" 
@@ -86,9 +100,11 @@
           class="img">
           <p class="text">Phanteks NV-Serie NV5 White</p>
           <p>479,00 zł</p>
+        </a>
       </div>
 
       <div class="product">
+        <a href="https://www.x-kom.pl/p/1152871-mikrofon-rode-nt1-5th-gen-black-mikrofon-pojemnosciowy.html">
         <img 
           src="https://cdn.x-kom.pl/i/setup/images/prod/big/product-medium,,2023/6/pr_2023_6_26_15_14_19_493_00.jpg" 
           loading="lazy" 
@@ -96,6 +112,7 @@
           lass="img">
           <p>Rode NT1 5th Gen Black - Mikrofon</p>
           <p>1 249,00 zł</p>
+        </a>
       
       </div>
     </div>
